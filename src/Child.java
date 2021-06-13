afc7134635e5fbb1a5fc52c06469c3f4fa6a8527
RakeshSudi <85624934+RakeshSudi@users.noreply.github.com>
2021-06-13 21:37:39 +1000

    Single Inheritance

---
+++ src/Child.java
@@ -0,0 +1,9 @@
+public class Child extends Parent{
+    public void printChildName() {
+        System.out.println("THis is the child class");
+    }
+
+    public void printName() {
+        System.out.println("Print name from Child");
+    }
+}
---
+++ src/Parent.java
@@ -0,0 +1,5 @@
+public class Parent {
+    public void printName() {
+        System.out.println("This is parent");
+    }
+}
